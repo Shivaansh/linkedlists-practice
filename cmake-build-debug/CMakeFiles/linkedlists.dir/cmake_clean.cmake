eb932c9af13effe4842a953462d7d92efa070dd6
Shiv <shivaanshseal@gmail.com>
2020-06-02 17:09:08 -0400

    Linked Lists practice project

---
+++ cmake-build-debug/CMakeFiles/linkedlists.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linkedlists.dir/main.c.o"
+  "liblinkedlists.dll.a"
+  "linkedlists.exe"
+  "linkedlists.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/linkedlists.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+
+//struct for a Linked List Node
+struct Node{
+    int value;
+    struct Node *next;
+};
+
+struct Node* createList(int arr[], int arrSize){
+    struct Node *first, *last, *temp;
+    int i = 0;
+    first = (struct Node*)malloc(sizeof(struct Node));
+    first->value = arr[0];
+    first->next = NULL;
+    last = first;
+
+    for(i=1; i<arrSize; i++){
+        temp = (struct Node*)malloc(sizeof(struct Node));
+        temp->value = arr[i];
+        temp->next = NULL;
+        last->next = temp;
+        last = temp;
+    }
+
+    return first;
+}
+
+void displayList(struct Node *p) {
+    while(p){
+        printf(" Node value: %d\n", p->value);
+        p = p->next;
+    }
+}
+
+void displayListRecursive(struct Node *p) {
+    //not while since last node would keep printing and never quit
+    if(p != NULL){
+        printf(" Node value: %d\n", p->value);
+        displayListRecursive(p->next);
+    }
+}
+
+void displayListReverse(struct Node *p) {
+    //not while since last node would keep printing and never quit
+    if(p != NULL){
+        displayListReverse(p->next);
+        printf(" Node value: %d\n", p->value);
+    }
+}
+
+int countNodesInList(struct Node *p){
+    int count = 0;
+    while(p != NULL){
+        count++;
+        p = p->next;
+    }
+    return count;
+}
+
+int sumNodesInList(struct Node *p){
+    int sum = 0;
+    while(p != NULL){
+        sum += p->value;
+        p = p->next;
+    }
+    return sum;
+}
+
+int maxInList(struct Node *p){
+    int max = -32768;
+    while(p != NULL){
+        if(max < p->value){
+            max = p->value;
+        }
+        p = p->next;
+    }
+    return max;
+}
+
+int main() {
+
+    int arr[6] = {2,4,6,8,10,12};
+    struct Node *p = createList(arr, 6);
+    printf("Iterative\n");
+    displayList(p);
+    printf("Recursive\n");
+    displayListRecursive(p);
+    printf("Reverse\n");
+    displayListReverse(p);
+    printf("Count: %d\n", countNodesInList(p));
+    printf("Sum: %d\n", sumNodesInList(p));
+    printf("Max: %d\n", maxInList(p));
+    return 0;
+}
+
+
